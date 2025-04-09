01c00aed00a0a69a497cbecc4a25fc990f2676b8
jooeun921 <jooeun921@gmail.com>
2025-04-09 17:14:20 +0900

    성공!

--- src/models/recruitment.py
+++ src/models/recruitment.py
@@ -31,3 +31,26 @@ class RecruitSite(Base):
     
     def __repr__(self):
         return f"<Site(site_id={self.site_id}, site_name={self.site_name})>"
+
+
+# 음식 테이블
+class Food(Base):
+    """food 테이블에 대한 모델"""
+    __tablename__ = 'food'
+    __table_args__ = {
+        'schema': 'recruitment',
+        'mysql_engine': 'InnoDB',
+        'mysql_charset': 'utf8',
+        'mysql_collate': 'utf8_general_ci',
+    }
+
+    food_id = Column(Integer, primary_key=True, autoincrement=False)
+    food_name = Column(String(40), nullable=False)
+    food_info = Column(String(40), nullable=False)
+    food_cal = Column(Integer, nullable=False)
+    food_rename = Column(Integer, nullable=True)
+    gone = Column(Boolean, nullable=False, server_default=text('true'))
+    make_date = Column(DateTime, nullable=False, server_default=text('CURRENT_TIMESTAMP'))
+
+    def __repr__(self):
+        return f"<Food(food_id={self.food_id}, food_name={self.food_name})>"
