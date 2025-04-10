8cfcf342a421805d995b50beeccf509337247011
zer0ken <a37592777@gmail.com>
2025-04-10 15:01:22 +0900

    `ui-logic`에 `db` 브랜치를 병합 (#7)
    * cert directory to .gitignore
    
    * Add recruit_site table to db
    
    * Initial db migration
    
    * revision
    
    * 테이블 추가
    
    * 테이블 생성
    
    * 성공!
    
    * user 테이블 추가
    
    * 업데이트
    
    * study_thread_id 추가
    
    * 유저
    
    * thread_id 수정
    
    * password 오타 수정
    
    * 리드 함수수
    
    * Fix SSL connection
    
    * CURD 추가
    
    * prettier 돌림
    
    ---------
    
    Co-authored-by: hadenkr <hadenkr@gmail.com>
    Co-authored-by: jooeun921 <jooeun921@gmail.com>
    Co-authored-by: Ju-hong <luyenen@gmail.com>
    Co-authored-by: wjdls001 <smw08098@gmail.com>

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+# from distutils.util import strtobool
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -7,13 +8,15 @@ class Config:
     DEBUG = False
     TESTING = False
     
-    VERSION_NUMBER = "1.0"
+    VERSION_NUMBER = "1.1"
     
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     SQLALCHEMY_DB_DRIVER = os.environ.get('SQLALCHEMY_DB_DRIVER') or 'mysql+pymysql'
-    SQLALCHEMY_USE_SSL = bool(os.environ.get('SQLALCHEMY_USE_SSL', 'False'))
+    # SQLALCHEMY_USE_SSL = bool(os.environ.get('SQLALCHEMY_USE_SSL', 'False'))
+    # SQLALCHEMY_USE_SSL = strtobool(os.environ.get('SQLALCHEMY_USE_SSL', 'False'))
+    SQLALCHEMY_USE_SSL = True if os.environ.get('SQLALCHEMY_USE_SSL', 'False') == 'True' else False
     SQLALCHEMY_DB_CERT = os.environ.get('SQLALCHEMY_DB_CERT')
     SQLALCHEMY_DB_USER = os.environ.get('SQLALCHEMY_DB_USER', 'user')
     SQLALCHEMY_DB_PASS = os.environ.get('SQLALCHEMY_DB_PASS', 'pass')
