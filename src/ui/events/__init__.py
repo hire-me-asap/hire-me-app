9118046971f48fb5bd2e93aa7cc527c88849ff5a
Bosongsae <volavish@gmail.com>
2025-04-16 12:41:57 +0900

    events 의 init 수정

--- src/ui/events/__init__.py
+++ src/ui/events/__init__.py
@@ -3,3 +3,6 @@ from .on_demo_load import update_sidebar_profile_image, load_histories
 from .on_input_submit import queue_message, wait_message
 from .on_sidebar_expand_or_collapse import set_topbar_visibility
 from .on_tab_change_required import select_profile_tab, select_chat_tab
+from .on_add_row import add_row 
+from .on_call_update_resume_info import call_update_resume_info
+from .on_generate_user_info import generate_user_info_json
