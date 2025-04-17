a3163b380bed2a3099291de67c55034650747b38
hadenkr <hadenkr@gmail.com>
2025-04-17 18:40:03 +0900

    Modify session_middleware

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ app.add_middleware(
 async def session_middleware(request: Request, call_next):
     session_id = get_session_id(request)
     response = await call_next(request)
-    response.set_cookie(key="session_id", value=session_id)
+    response.set_cookie(key="session_id", value=session_id, secure=True, httponly=True, samesite="None")
     return response
 
 # FastAPI 엔드포인트로 세션 정보 확인
