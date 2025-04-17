faf960a59bb91b787b8902beecd7e6f0a81ead5e
Bosongsae <volavish@gmail.com>
2025-04-17 12:33:25 +0900

    key error 해결

--- src/ui/events/on_json_to_user_component.py
+++ src/ui/events/on_json_to_user_component.py
@@ -14,12 +14,12 @@ def json_to_user_component(user_db) :
     else :
         summary = ""
    
-    if user_db['skill_stack'] :
+    if user_db.get('skill_stack') :
         skill_stack = user_db['skill_stack']
     else :
         skill_stack = ""
 
-    if user_db['education'] :
+    if user_db.get('education') :
         final_degree = user_db['education'][0]['final_degree']
         major = user_db['education'][0]['major']
         school_name = user_db['education'][0]['school_name']
@@ -32,27 +32,27 @@ def json_to_user_component(user_db) :
         gpa = ""
         degree_date = ""
 
-    if user_db['education_and_exp'] :
+    if user_db.get('education_and_exp') :
         education_exp = pd.DataFrame(user_db['education_and_exp'])
     else :
         education_exp = pd.DataFrame(columns=['교육명', '기간 (YYYY.MM - YYYY.MM)'])
 
-    if user_db['work_experiences'] :
+    if user_db.get('work_experiences') :
         work_experiences = pd.DataFrame(user_db['work_experiences'])
     else :
         work_experiences = pd.DataFrame(columns=['회사명','근무기간 (YYYY.MM - YYYY.MM)' , '직책', '주요 업무'])     
 
-    if user_db['certificates'] :
+    if user_db.get('certificates') :
         cerificates = pd.DataFrame(user_db['certificates'])
     else :
         cerificates = pd.DataFrame(columns=['자격증명', '취득일 (YYYY.MM.DD)']) 
 
-    if user_db['awards'] :
+    if user_db.get('awards') :
         awards = pd.DataFrame(user_db['awards'])
     else :
         awards = pd.DataFrame(columns=['수상명', '수상일 (YYYY.MM.DD)']) 
 
-    if user_db['languages'] :
+    if user_db.get('languages') :
         languages = pd.DataFrame(user_db['languages'])
     else :
         languages = pd.DataFrame(columns=['어학시험/점수', '취득일 (YYYY.MM.DD)']) 
